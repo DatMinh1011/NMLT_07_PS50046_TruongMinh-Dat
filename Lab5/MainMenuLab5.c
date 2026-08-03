89aa639e824c747fe165285b306cc395b005d6f2
Minh Dat <vumind2000@gmail.com>
2026-08-03 19:01:18 +0700

    HT lan5

---
+++ Lab5/MainMenuLab5.c
@@ -0,0 +1,161 @@
+#include <stdio.h>
+#include <stdbool.h>
+void menu();
+void chucNang1();
+int soLonNhat(int a, int b, int c);
+void chucNang2();
+bool kiemTraNamNhuan(int nam);
+void chucNang3();
+void hoanVi(int *a, int *b);
+void chucNang4();
+const char *phanLoaiTamGiac(float a, float b, float c);
+int main()
+{
+    int chon;
+    do
+    {
+        menu();
+        printf("Xin moi chon chuc nang (1-5): ");
+        scanf("%d", &chon);
+        switch (chon)
+        {
+        case 1:
+            chucNang1();
+            break;
+        case 2:
+            chucNang2();
+            break;
+        case 3:
+            chucNang3();
+            break;
+        case 4:
+            chucNang4();
+            break;
+        case 5:
+            printf("Thoat chuong trinh.\n");
+            break;
+        default:
+            printf("Lua chon khong hop le. Vui long chon lai.\n");
+            break;
+        }
+    } while (chon != 5);
+    return 0;
+}
+void menu()
+{
+    printf("+---------------------------------------------------+\n");
+    printf("|             MENU CHUONG TRINH LAB 5               |\n");
+    printf("+---------------------------------------------------+\n");
+    printf("| 1. Tim gia tri lon nhat trong 3 so                |\n");
+    printf("| 2. Kiem tra nam nhuan                             |\n");
+    printf("| 3. Hoan vi 2 so (Su dung Con tro)                 |\n");
+    printf("| 4. Kiem tra & Phan loai Tam giac                  |\n");
+    printf("| 5. Thoat chuong trinh                             |\n");
+    printf("+---------------------------------------------------+\n");
+}
+void chucNang1()
+{
+    int a, b, c;
+    printf("Nhap a, b, c: ");
+    scanf("%d %d %d", &a, &b, &c);
+    printf("Gia tri lon nhat trong 3 so vua nhap la: %d\n", soLonNhat(a, b, c));
+}
+
+int soLonNhat(int a, int b, int c)
+{
+    int max = a;
+    if (b > max)
+    {
+        max = b;
+    }
+    if (c > max)
+    {
+        max = c;
+    }
+    return max;
+}
+void chucNang2()
+{
+    int nam;
+    printf("Chuc nang 2: Kiem tra nam nhuan\n");
+    printf("Nhap nam: ");
+    scanf("%d", &nam);
+    if (kiemTraNamNhuan(nam))
+    {
+        printf("Nam %d la nam nhuan.\n", nam);
+    }
+    else
+    {
+        printf("Nam %d khong phai la nam nhuan.\n", nam);
+    }
+}
+
+bool kiemTraNamNhuan(int nam)
+{
+    if ((nam % 4 == 0 && nam % 100 != 0) || (nam % 400 == 0))
+    {
+        return true;
+    }
+    return false;
+}
+void chucNang3()
+{
+    int a, b;
+    printf("Chuc nang 3: Hoan vi 2 so (Su dung Con tro)\n");
+    printf("Nhap 2 so nguyen: ");
+    scanf("%d %d", &a, &b);
+    printf("Truoc khi hoan vi: a = %d, b = %d\n", a, b);
+    hoanVi(&a, &b);
+    printf("Sau khi hoan vi: a = %d, b = %d\n", a, b);
+}
+
+void hoanVi(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void chucNang4()
+{
+    printf("Chuc nang 4: Kiem tra & Phan loai Tam giac\n");
+    float a, b, c;
+    printf("Nhap 3 canh cua tam giac: ");
+    scanf("%f %f %f", &a, &b, &c);
+    const char *loai = phanLoaiTamGiac(a, b, c);
+    printf("Tam giac vua nhap la: %s\n", loai);
+}
+
+const char *phanLoaiTamGiac(float a, float b, float c)
+{
+    if (a + b > c && a + c > b && b + c > a && a > 0 && b > 0 && c > 0) 
+    {
+        if (a == b && b == c)
+        {
+            return "Tam giac deu";
+        }
+        else if (a == b || a == c || b == c)
+        {
+            return "Tam giac can";
+        }
+        else if ((a * a + b * b == c * c) || (a * a + c * c == b * b) || (b * b + c * c == a * a))
+        {
+            if (a == b || a == c || b == c)
+            {
+                return "Tam giac vuong can";
+            }
+            else
+            {
+                return "Tam giac vuong";
+            }
+        }
+        else
+        {
+            return "Tam giac thuong";
+        }
+    }
+    else
+    {
+        return "Khong phai tam giac";
+    }
+    return "Khong phai bo 3 canh cua tam giac";
+}
